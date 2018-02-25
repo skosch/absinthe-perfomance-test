bd456c81c7c5ad9d69c0eff690f9de4832ca248f
Ben Wilson <benwilson512@gmail.com>
2018-02-24 22:53:47 -0500

    move data outside absinthe execution

--- lib/app.ex
+++ lib/app.ex
@@ -13,8 +13,14 @@ defmodule App do
       }
     """
 
+    {time, data} = :timer.tc(fn ->
+      :erlang.binary_to_term(File.read!("./data"))
+    end)
+
+    IO.inspect("Loaded data file in #{inspect(time / 1000)} ms")
+
     {time, _result} = :timer.tc(fn ->
-      Absinthe.run(query_string, App.Schema)
+      Absinthe.run(query_string, App.Schema, root_value: %{data: data})
     end)
 
     IO.inspect("Ran absinthe query in #{inspect(time / 1000)} ms")
